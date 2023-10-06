623170b284c87518c1ffbfc775958f097e8325aa
kxs-sazrin <sazrin@kinaxis.com>
2023-10-06 15:58:42 -0400

    added todo schema

---
+++ schema/todoSchema.js
@@ -0,0 +1,34 @@
+// store mongoose schema and model. So, a blueprint for our tasks for how they will look.
+
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+
+const TodoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    detail: {
+        type: String,
+        required: true,
+        enum: ['planned', 'in progress', 'completed'],
+        default: 'planned',
+        trim: true
+    },
+    code: {
+        type: String,
+        required: true,
+        default: 'code',
+        trim: true
+    }
+}, {timestamps: true})
+
+
+TodoSchema.pre('save', function(next){
+    this.code = nanoid(10);
+    next();
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+export default Todo;
